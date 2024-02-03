2135066e6ea0e81a984bb47897dfd0b525c84b8f
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-02-03 18:36:14 -0300

    Init: Web Crawler

---
+++ Robo.py
@@ -0,0 +1,13 @@
+import sys
+
+class Robo():
+
+    def __init__(self) -> None:
+        pass
+
+    def chama_nome_robo(nome: str):
+        print(nome)
+
+if __name__ == "__main__":
+    Robo.chama_nome_robo("vm-01")
+    
