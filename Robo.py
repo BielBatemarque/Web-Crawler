4f6b270a64bbc17a6b467732b85c0ff3b34394b5
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-02-18 22:45:57 -0300

    feat: leitura do cfg

--- Robo.py
+++ Robo.py
@@ -1,4 +1,5 @@
 from utils.logger import Logger
+from utils.config import Config
 import sys
 
 
@@ -9,6 +10,11 @@ class Robo():
 
     def chama_nome_robo(nome: str):
         Logger.debug(nome)
+
+
+    site = Config.retorna_preferenciais_de_busca()["marketplace"]
+    print(site)
+    
 
 if __name__ == "__main__":
     Robo.chama_nome_robo("vm-01")
