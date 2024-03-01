da2becb25115844b3a38168654cca4349de790c6
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-03-01 18:00:17 -0300

    Feat: enviando request

--- models/buscador.py
+++ models/buscador.py
@@ -1,15 +1,32 @@
 from .Loja import Loja
-from requests import requests
+import requests
 
 class Buscador():
 
     def __init__(self, loja:Loja) -> None:
         self.loja = loja
+        self.session = requests.Session()
+        self.requisicao = None
+        self.resposta = None
 
     
-    def abrir_requisisao(link, method, headers, data):
-        req = requests.send(
-            
+    def abrir_requisisao(self, link, method="GET", headers=None, data=None, json=None):
+        self.requisicao = requests.Request(
+            method=method,
+            url=link,
+            data=data,
+            json=json
+        )
+
+    def enviar(self, permitir_redirecionamentos=True, stream_de_dados=False, **Kwargs):
+        request_final = self.session.prepare_request(self.requisicao)
+
+        self.resposta = self.session.send(
+            request_final,
+            allow_redirects=permitir_redirecionamentos,
+            stream=stream_de_dados,
+            verify=False,
+            **Kwargs
         )
 
     def buscar():
