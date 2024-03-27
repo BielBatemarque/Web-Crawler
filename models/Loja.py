bdf8e417292164392ac61269cabcaaf7dd973e84
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-03-27 13:53:09 -0300

    Fix: Classe de Loja

--- models/Loja.py
+++ models/Loja.py
@@ -1,4 +1,10 @@
-class Loja():
+from abc import ABC, abstractclassmethod
+from models.resposta import Resposta
+class Loja(ABC):
+    NOME_LOJA = str
 
-    def __init__(self) -> None:
-        pass
+
+    def __init__(self, nome, url) -> None:
+        self.nome = nome
+        self.url = url
+        self.resultado: Resposta = Resposta()
