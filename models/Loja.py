83a8fea34aea7303627d06d5f778c8dc93e31202
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-04-07 00:09:43 -0300

    fix : espaçamento de lojas

--- models/Loja.py
+++ models/Loja.py
@@ -2,8 +2,7 @@ from abc import ABC, abstractclassmethod
 from models.resposta import Resposta
 class Loja(ABC):
     NOME_LOJA = str
-
-
+    
     def __init__(self, nome, url) -> None:
         self.nome = nome
         self.url = url
