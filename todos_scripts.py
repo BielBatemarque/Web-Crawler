c66cd54bab6b83924877ac0e7ec47663874374cc
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-04-07 00:09:32 -0300

    feat: todos_scripts

---
+++ todos_scripts.py
@@ -0,0 +1,9 @@
+from scripts.americanas import Americanas
+from scripts.olx import Olx
+from scripts.submarino import Submarino
+
+class TodosScripts():
+
+    scripts = [
+        Americanas, Olx, Submarino
+    ]
